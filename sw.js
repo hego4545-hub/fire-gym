ab6881e2b60bbd7810a70df1754ee997475edf70
hego4545-hub <hego4545@gmail.com>
2026-04-13 05:03:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for Fire Gym Mobile
-const CACHE_NAME = 'fire-gym-v1';
+// Service Worker for Fire Gym Mobile - Version 2
+const CACHE_NAME = 'fire-gym-v2';
 const assets = [
   './',
   './index.html',
@@ -7,17 +7,28 @@ const assets = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
     })
   );
 });
